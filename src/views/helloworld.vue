cb8248a479be24ea13fa98c98fdf619c951f1613
ExodusMaksim <102152854@qq.com>
2022-06-28 18:05:37 +0800

    初始化仓库

---
+++ src/views/helloworld.vue
@@ -0,0 +1,34 @@
+<template>
+  <p class="msg">{{ msg }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue'
+import Child from "@cp/Child.vue"
+
+export default defineComponent({
+  components: {
+    Child
+  },
+  setup() {
+    const msg = ref<HTMLElement | null>(null)
+    const child = ref<typeof Child | null>(null)
+
+    onMounted(() => {
+      console.log(msg.value?.innerText)
+      child.value.isShowDialog = true
+    })
+
+    return {
+      msg,
+      child,
+    }
+  },
+})
+</script>
+
+<style scoped>
+.msg {
+  font-size: 14px;
+}
+</style>
